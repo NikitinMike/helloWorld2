d39050e51d589a7703558d0b703f3cc863a00cf3
Mikhail Nikitin <36443086+NikitinMike@users.noreply.github.com>
2024-09-19 09:28:17 +0300

    Initial Commit

---
+++ helloWorld2/helloWorld2App.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorld2App.swift
+//  helloWorld2
+//
+//  Created by МИХАИЛ on 19.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct helloWorld2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ helloWorld2Tests/helloWorld2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorld2Tests.swift
+//  helloWorld2Tests
+//
+//  Created by МИХАИЛ on 19.09.2024.
+//
+
+import Testing
+@testable import helloWorld2
+
+struct helloWorld2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
